774e4c2bf0ab763b9d4fc78f3746bf403e980446
Darya Samoylova <63449321+darya-samoylova@users.noreply.github.com>
2020-10-23 17:26:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Создаем переменную, в которую положим кнопку меню
+let menuToggle = document.querySelector('#menu-toggle');
+//Создаем переменную, в которую положим меню
+let menu = document.querySelector('.sidebar');
+
+//Отслеживаем клик по кнопке меню и запускаем функцию
+menuToggle.addEventListener('click', function (event) {
+   //Отменяем стандартное поведение сслыки
+   event.preventDefault(); 
+   //Вешаем класс на меню, когда кликнули по кнопке меню
+   menu.classList.toggle('visible');
+})
